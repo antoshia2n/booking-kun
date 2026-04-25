a92d25f660b26186a65c36b508c93d90da30ded7
antoshia2n <gameister1@gmail.com>
2026-04-25 23:52:31 +0900

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,2 @@
-// ← このファイルを各アプリで書き換える
-export const APP_ID   = "my-app";       // Firestore allowedApps で使われるID
-export const APP_NAME = "My App";       // ログイン画面に表示される名前
-
-export const TABS = ["ホーム", "設定"];
+export const APP_ID   = "bk";
+export const APP_NAME = "予約くん";
